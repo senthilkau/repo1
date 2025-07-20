33e8fc9e71f393e9c1c27cf2aa15472dc9f3ec86
senthilkau <senthil@hotmail.co.nz>
2025-07-20 18:24:02 +1000

    remove rg

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-
+/*
 resource "azurerm_resource_group" "example" {
   name     = "tf-sample-rg"
   location = "australiaeast"
 }
-/*
+
 resource "azurerm_virtual_network" "example" {
   name                = "tf-sample-vnet"
   address_space       = ["10.0.0.0/16"]
