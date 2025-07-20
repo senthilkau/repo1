15a76c5c7d22b3786738ca0dfc76dbde519d0cad
senthilkau <senthil@hotmail.co.nz>
2025-07-20 12:06:25 +1000

    Create new VM

--- main.tf
+++ main.tf
@@ -1,6 +1,61 @@
-/*
 resource "azurerm_resource_group" "example" {
   name     = "tf-sample-rg"
   location = "australiaeast"
 }
-*/
+
+resource "azurerm_virtual_network" "example" {
+  name                = "tf-sample-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "tf-sample-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "tf-sample-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    // No public_ip_address_id specified
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "tf-sample-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_password = "P@ssword1234!" // For demo only; use SSH keys in production
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "tf-sample-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  disable_password_authentication = false
+}
+
+// No public IP resource is created or
