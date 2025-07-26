86c14f746e3cb4e046020c56439506fa442c48b7
senthilkau <senthil@hotmail.co.nz>
2025-07-26 18:41:42 +1000

    sync code

--- import.tf
+++ import.tf
@@ -1,4 +1,6 @@
+/*
 import {
   to = azurerm_storage_account.this
   id = "/subscriptions/1b71a911-fba4-4f36-80db-f4d30f1f9842/resourceGroups/rg-demo/providers/Microsoft.Storage/storageAccounts/w356qi"
 }
+*/
