9a6adb225c9d17dab9e84374effeaa1f9c1af8eb
senthilkau <senthil@hotmail.co.nz>
2025-07-26 15:11:19 +1000

    create storage account

---
+++ storage.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1. Create a resource group for the storage account
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo"
+  location = "australiaeast"
+}
+
+# 2. Instantiate your private module
+module "storage" {
+  source  = "app.terraform.io/senthilkau/storage-account/azurerm"
+  version = "0.1.1"
+
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# 3. Optional: expose outputs
+output "storage_account_id" {
+  value = module.storage.storage_account_id
+}
+
+output "primary_blob_endpoint" {
+  value = module.storage.primary_blob_endpoint
+}
