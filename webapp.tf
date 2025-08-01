1d356fd399517fa68841d7ac098e804e9812ac79
Senthil Kumar <Senthil.Kumar@brisbane.qld.gov.au>
2025-08-01 15:27:38 +1000

    update code for webapp

--- webapp.tf
+++ webapp.tf
@@ -7,23 +7,25 @@ resource "azurerm_app_service_plan" "webapp_plan" {
   name                = "webapp-service-plan"
   location            = azurerm_resource_group.webapp_rg.location
   resource_group_name = azurerm_resource_group.webapp_rg.name
+  kind                = "Linux"
   sku {
     tier = "Basic"
     size = "B1"
   }
 }
 
-resource "azurerm_web_app" "webapp" {
-  name                = "basic-webapp"
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "basic-linux-webapp"
   location            = azurerm_resource_group.webapp_rg.location
   resource_group_name = azurerm_resource_group.webapp_rg.name
-  app_service_plan_id = azurerm_app_service_plan.webapp_plan.id
+  service_plan_id     = azurerm_app_service_plan.webapp_plan.id
 
   site_config {
     scm_type = "LocalGit"
+    always_on = true
   }
 
   https_only = true
 }
 
-# No public IP is exposed by default for Azure Web
+# No public IP is exposed by default for Azure Web Apps
